6b49c1210c9ae3fc48ea98210421feda4cdd3b35
young_seok_Go <109319895+YoungSeok222222@users.noreply.github.com>
2022-07-21 19:37:42 +0900

    Add files via upload

---
+++ 7.20.Wed/use.py
@@ -0,0 +1,8 @@
+# import module
+from module import plus as module1_plus
+from module2 import plus as module2_plus
+
+# print(module.plus(1, 2))
+# print(plus(1, 2))
+print(module1_plus(1, 2))
+print(module2_plus(1, 2))
