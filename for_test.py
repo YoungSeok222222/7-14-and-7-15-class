ed458b26e874b139032b893b990021f9d7990af1
Young <youngseokgo26@naver.com>
2022-07-21 19:27:10 +0900

    dd

---
+++ for_test.py
@@ -0,0 +1,32 @@
+#리스트에서 index(색인) 하는 법-> 변수[위치]
+levels = [1415,1430,1475,1490]
+print(levels[0])
+print(levels[1])
+print(levels[2])
+print(levels[3])
+
+# for문 정해진 범위를 반복하기에 종료조건이 필요 없음
+for x in levels:
+    print(x)
+ 
+ 
+greeting = '안녕하세요'   
+#while 
+count = 0
+while count< 4:
+    print(greeting)
+    count+=1
+    
+#for 
+for x in range(4):
+    print(greeting)
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
