bd5a34919223d5971d4cac25fa2526cbb7098d9e
Young <youngseokgo26@naver.com>
2022-08-29 23:23:33 +0900

    TIL

--- qqqqqqqqqqqq.py
+++ qqqqqqqqqqqq.py
@@ -1,23 +1,38 @@
-T = int(input())
-for t in range(T):
-    N = int(input())
-    bus = [list(map(int,input().split())) for _ in range(N)]
-    bus_stop = [0] * 5001
+N = int(input())
 
-    p = int(input())
-
-    idx = []
-    for i in range(p):
-        idx.append(int(input()))
-
-    for i in bus:
-        for j in range(i[0],i[1]+1):
-            bus_stop[j] += 1
-    print(f"#{t+1}",end=' ')
-    for x in idx:
-        print(bus_stop[x],end=' ')
-    print()
-    #
+arr = [list(map(int,input().split())) for _ in range(N)]
+farm = [[0]*3 for _ in range(3)]
 
 
+crop = []
+for i in range(len(arr)):                        # 농작물 따로 보관
+    crop.append(int(str(arr[i].pop())))
 
+for i in range(len(arr)):                        # 농작물 농장에 심기
+    farm[arr[i][0]][arr[i][1]] = crop[i]
+# print(farm)
+
+t = int(input())
+typhoon = list(map(int,input().split()))         # 태풍 발생
+
+for p in typhoon:
+    for y in range(3):
+        for x in range(3):
+            if farm[y][x] ==0: continue
+            elif farm[y][x]%10 > p:              # 태풍에 유실
+                farm[y][x] -= p
+            else:
+                farm[y][x] = farm[y][x] // 10
+
+result = []
+
+for y in range(3):
+    for x in range(3):
+        if farm[y][x]:
+            result.append(str(farm[y][x]))
+
+print(len(''.join(result)))
+
+
+
+# arr = [input() for _ in range(N)]
