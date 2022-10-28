6b58c491c0f210c3943e4afa387486590c52d569
YoungSeok222222 <youngseokgo26@naver.com>
2022-10-28 17:58:47 +0900

    TIL

--- 220919.py
+++ 220919.py
@@ -1,6 +1,16 @@
-N = input()
+N = int(input())
 
-Sum =  0 
-for i in N:
-    print(i)
+ar = [list(map(int,input().split())) for _ in range(N)]
+re = []
+print(ar)
+
+
+for i in range(N):
+    num = 1
+    for j in range(N):
+        if ar[i][0] < ar [j][0] and ar[i][1] < ar[j][1]: num += 1
+    re.append()
+        
+
+
 
