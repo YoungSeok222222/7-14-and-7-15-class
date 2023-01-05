3b5878d52d14620fd71451585f0556886256d2ae
Young <youngseokgo26@naver.com>
2023-01-06 00:01:17 +0900

    java update

---
+++ 01-05-Thurs/_43_생성자_this_참조변수_this.java
@@ -0,0 +1,33 @@
+public class _43_생성자_this_참조변수_this {
+    public static void main(String[] args) {
+// ch6-36 생성자 this()
+        // - 생성자에서 다른 생성자 호출할 때 사용
+        // - 다른 생성자 호출시 첫 줄에서만 사용가능!!!!!!!!
+
+// 생성자가 하는 일은 iv 초기화
+
+
+/*
+class Car {
+    String color;
+    String gearType;
+    int door;
+                                    *코드의 중복을 제거한 코드
+    Car() {                             Car() {
+        color = "white";                    // Car("white", "auto", 4);
+        gearType = "auto";      =>          this("white", "auto", 4);
+        door = 4;
+    }
+
+    Car (String c, String g, int d) {
+         color = c;
+         gearType = g;
+         door = d;
+    }
+}
+*/
+
+        // 68 5:00
+
+    }
+}
